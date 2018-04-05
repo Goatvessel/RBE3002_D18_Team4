a33d19000dd713586bb2465e555b82c7da1d0a24
aimperiali <aimperiali@wpi.edu>
2018-04-05 18:20:54 -0400

    So much progress

--- src/rbe3002/scripts/RBE3002_2018D_Lab3_Grid_Cells.py
+++ src/rbe3002/scripts/RBE3002_2018D_Lab3_Grid_Cells.py
@@ -36,8 +36,28 @@ def mapCallBack(data):
 def readGoal(goal):
     global goalX
     global goalY
+    global goalPub
     goalX= goal.pose.position.x
     goalY= goal.pose.position.y
+
+    #finds the start within the cell boundaries
+    actX = goalX/1
+
+    goalCell = GridCells()
+    goalCell.header.frame_id = 'map'
+    goalCell.cell_width=resolution
+    goalCell.cell_height=resolution
+    point = Point()
+    point.x = goalX
+    point.y = goalY
+    # point.x = 11.15
+    # point.y = 10.65
+    point.z = 0
+    goalCell.cells.append(point)
+    #print(goalCell)
+    goalPub.publish(goalCell)
+
+
     print goal.pose
 
     # Start Astar
@@ -47,15 +67,24 @@ def readStart(startPos):
 
     global startPosX
     global startPosY
+    global startPub
     startPosX = startPos.pose.pose.position.x
     startPosY = startPos.pose.pose.position.y
+
     startCell = GridCells()
+    startCell.header.frame_id = 'map'
     startCell.cell_width=resolution
     startCell.cell_height=resolution
     point = Point()
-    point.
-    startCell.
-    publishCells(startCell)
+    point.x = round(startPos.pose.pose.position.x,0)
+    point.y = round(startPos.pose.pose.position.y,0)
+    # point.x = 11.15
+    # point.y = 10.65
+    point.z = 0
+    startCell.cells.append(point)
+    print(startCell)
+    startPub.publish(startCell)
+    #publishCells(startCell)
     print startPos.pose.pose
 
 def aStar(start,goal):
@@ -83,34 +112,42 @@ def publishCells(grid):
     cells.cell_width = resolution
     cells.cell_height = resolution
 
-    for i in range(1,height): #height should be set to hieght of grid
-        for j in range(1,width): #width should be set to width of grid
-            if(grid[k] == 100):
-                point = Point()
-                point.x = (j*resolution) + offsetX + (.5*resolution)
-                point.y = (j*resolution) + offsetY + (.5*resolution)
-                point.z = 0
+    for i in range(0,height): #height should be set to hieght of grid
+        for j in range(0,width): #width should be set to width of grid
+
+            # grid values are probability percentages of occupancy. The following condition gives all the cells where
+            # occupancy probability is more than 50%
+            if (grid[k] > 50):
+                point=Point()
+                # 0.5*resolution moves the point to the center of the grid cell
+                point.x=(j*resolution)+offsetX + (0.5 * resolution)
+                point.y=(i*resolution)+offsetY + (0.5 * resolution)
+                point.z=0
                 cells.cells.append(point)
-            k=k+1
+            k = k + 1
     pub.publish(cells)
 
 #Main handler of the project
 def run():
     global pub
+    global startPub
+    global goalPub
     rospy.init_node('lab3')
     sub = rospy.Subscriber("/map", OccupancyGrid, mapCallBack)
     pub = rospy.Publisher("/map_check", GridCells, queue_size=1)
+    startPub = rospy.Publisher("/start_cell", GridCells, queue_size=1)
+    goalPub = rospy.Publisher("/goal_cell", GridCells, queue_size=1)
     pubpath = rospy.Publisher("/path", GridCells, queue_size=1) # you can use other types if desired
     pubway = rospy.Publisher("/waypoints", GridCells, queue_size=1)
     goal_sub = rospy.Subscriber('move_base_simple/goal', PoseStamped, readGoal, queue_size=1) #change topic for best results
     goal_sub = rospy.Subscriber('initialpose', PoseWithCovarianceStamped, readStart, queue_size=1) #change topic for best results
 
     # wait a second for publisher, subscribers, and TF
-    rospy.sleep(5)
+    rospy.sleep(1)
 
 
 
-    while (1 and not rospy.is_shutdown()):
+    while (not rospy.is_shutdown()):
         publishCells(mapData) #publishing map data every 2 seconds
         rospy.sleep(2)
         print("Complete")
