1b098f62a13649b179e8f821fec88018f704f988
aimperiali <aimperiali@wpi.edu>
2018-04-05 09:56:00 -0400

    Minor Changes

--- src/rbe3002/scripts/RBE3002_2018D_Lab3_Grid_Cells.py
+++ src/rbe3002/scripts/RBE3002_2018D_Lab3_Grid_Cells.py
@@ -8,8 +8,10 @@ from nav_msgs.msg import Odometry, OccupancyGrid
 from kobuki_msgs.msg import BumperEvent
 import tf
 import numpy
-import math 
+import math
 import rospy, tf, numpy, math
+from Queue import PriorityQueue
+
 
 
 
@@ -57,6 +59,7 @@ def aStar(start,goal):
     # generate a path to the start and end goals by searching through the neighbors, refer to aStar_explanied.py
 
     # for each node in the path, process the nodes to generate GridCells and Path messages
+    while (not openSet.empty())
 
     # Publish points
 
@@ -70,7 +73,7 @@ def publishCells(grid):
     k=0
     cells = GridCells()
     cells.header.frame_id = 'map'
-    cells.cell_width = resolution 
+    cells.cell_width = resolution
     cells.cell_height = resolution
 
     for i in range(1,height): #height should be set to hieght of grid
@@ -80,18 +83,18 @@ def publishCells(grid):
             #print k # used for debugging
             if (grid[k] == 100):
                 point=Point()
-                point.x=(j*resolution)+offsetX + (1.5 * resolution) # added secondary offset 
+                point.x=(j*resolution)+offsetX + (1.5 * resolution) # added secondary offset
                 point.y=(i*resolution)+offsetY - (.5 * resolution) # added secondary offset ... Magic ?
                 point.z=0
                 cells.cells.append(point)
-    pub.publish(cells)           
+    pub.publish(cells)
 
 #Main handler of the project
 def run():
     global pub
     rospy.init_node('lab3')
     sub = rospy.Subscriber("/map", OccupancyGrid, mapCallBack)
-    pub = rospy.Publisher("/map_check", GridCells, queue_size=1)  
+    pub = rospy.Publisher("/map_check", GridCells, queue_size=1)
     pubpath = rospy.Publisher("/path", GridCells, queue_size=1) # you can use other types if desired
     pubway = rospy.Publisher("/waypoints", GridCells, queue_size=1)
     goal_sub = rospy.Subscriber('move_base_simple/goal', PoseStamped, readGoal, queue_size=1) #change topic for best results
@@ -104,9 +107,9 @@ def run():
 
     while (1 and not rospy.is_shutdown()):
         publishCells(mapData) #publishing map data every 2 seconds
-        rospy.sleep(2)  
+        rospy.sleep(2)
         print("Complete")
-    
+
 
 
 if __name__ == '__main__':
